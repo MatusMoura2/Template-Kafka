fefe9667ffc17b0675e1530e3b5bbbc195f48a91
Matus Moura <matus.moura333x2@gmail.com>
2024-08-16 17:09:16 -0300

    mf-priducer starter

---
+++ mf-producer/src/main/java/com/mouraforte/mfproducer/MfProducerApplication.java
@@ -0,0 +1,13 @@
+package com.mouraforte.mfproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfProducerApplication.class, args);
+	}
+
+}
---
+++ mf-producer/src/test/java/com/mouraforte/mfproducer/MfProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mouraforte.mfproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
