4df6166cb03bdf255d96b146136b0df544ab5fe9
Matus Moura <matus.moura333x2@gmail.com>
2024-08-25 23:56:13 -0300

    lista customizada @interface StringConsumerCustomListener

--- mf-consumer/src/main/java/com/mouraforte/mfconsumer/listeners/StringConsumerListener.java
+++ mf-consumer/src/main/java/com/mouraforte/mfconsumer/listeners/StringConsumerListener.java
@@ -3,14 +3,25 @@ package com.mouraforte.mfconsumer.listeners;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import com.mouraforte.mfconsumer.custom.StringConsumerCustomListener;
+
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Component
 public class StringConsumerListener {
 
-	@KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "stringContainerFactory")
-	public void listener(String message) {
-		log.info("Receive message {}",message);
+	@StringConsumerCustomListener(groupId = "group-1")
+	public void create(String message) {
+		log.info("Create ::: Receive message {}",message);
+	}
+	@StringConsumerCustomListener(groupId = "group-1")
+	public void log(String message) {
+		log.info("LOG ::: Receive message {}",message);
+	}
+	
+	@KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "stringContainerFactory")
+	public void log2(String message) {
+		log.info("History ::: Receive message {}",message);
 	}
 }
